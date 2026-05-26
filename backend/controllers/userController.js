d2c698c89bc6363acf0a86b430cf5eca9ee5b8db
Darshan G.V <darshangv36@gmail.com>
2026-05-27 01:09:46 +0530

    minor changes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+// Route for user login 
+
+ const loginUser = async()=>{
+
+
+ }
+
+  // Route for user registration
+
+  const registerUser = async()=>{
+
+  }
+
+  // Route for admin login
+
+  const adminLogin = async(req, res)=>{
+
+  }
+
+  export {loginUser, registerUser, adminLogin}
