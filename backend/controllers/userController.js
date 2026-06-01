59239275d04aff36b6c8f982c6340d496d1bb68f
Darshan G.V <darshangv36@gmail.com>
2026-06-01 23:46:33 +0530

    loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,9 +9,39 @@ import jwt from 'jsonwebtoken';
    }
 
  const loginUser = async(req,res)=>{
+   try{
 
+      const {email, password} = req.body;
 
- }
+      const user = await userModel.findOne({email});
+
+      if(!user){
+         return res.status(400).json({msg:"User does not exist"});
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if(!isMatch){
+         return res.status(400).json({msg:"Invalid credentials"});
+      }
+
+      const token = createToken(user._id);
+
+      res.status(200).json({
+         msg:"User logged in successfully",
+         token
+      });
+
+   } catch(error){
+
+      console.log(error);
+
+      res.status(500).json({
+         msg:"Error logging in user"
+      });
+   }
+}
+  
 
   // Route for user registration
 
