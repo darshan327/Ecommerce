5eb920b64aac4b049b413234a6b3b7de5c34876b
Darshan G.V <darshangv36@gmail.com>
2026-06-13 00:02:33 +0530

    some minor error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,13 +5,13 @@ const addProduct = async (req,res) => {
        try{
      const {name,description,price,category,subcategory,sizes,bestseller}=req.body;
 
-     const image1 = req.files.image1[0];
-     const image2 = req.files.image2[0];
-     const image3 = req.files.image3[0];
-     const image4 = req.files.image4[0];
+     const image1 = req.files.image1 && req.files.image1[0];
+     const image2 = req.files.image2 && req.files.image2[0];
+     const image3 = req.files.image3 && req.files.image3[0];
+   
 
      console.log(name,description,price,category,subcategory,sizes,bestseller)
-     console.log(image1,image2,image3,image4)
+     console.log(image1,image2,image3)
       
      res.status(200).json({message:"Product added successfully"})
   } catch(error){
